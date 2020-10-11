bf2b5a0bd7fd18bbfbd8d3a1d95f368679c2bb55
Justin Rager <jrager@ece023.ece.local.cmu.edu>
2020-10-11 16:17:37 -0400

    almost there

--- code.sv
+++ code.sv
@@ -79,6 +79,46 @@ module isWrongTester ();
 
 endmodule: isWrongTester
 
+module BCDtoSevenSegment 
+    (input logic [3:0] TotalHits,
+    output logic [6:0] numHits);
+
+    always_comb
+        case (TotalHits)
+            4'b0000: numHits = 7'b100_0000; //0
+            4'b0001: numHits = 7'b111_1001; //1
+            4'b0010: numHits = 7'b010_0100; //2 
+            4'b0011: numHits = 7'b011_0000; //3
+            4'b0100: numHits = 7'b001_1001; //4
+            4'b0101: numHits = 7'b001_0010; //5
+            4'b0110: numHits = 7'b000_0010; //6
+            4'b0111: numHits = 7'b111_1000; //7
+            4'b1000: numHits = 7'b000_0000; //8
+            4'b1001: numHits = 7'b001_0000; //9
+            default: numHits = 7'b111_1111; //all off
+    endcase
+endmodule: BCDtoSevenSegment
+
+module BCDtoSevenSegment_test();
+    logic [3:0] TotalHits;
+    logic [6:0] numHits;
+
+    BCDtoSevenSegment bss(.TotalHits(TotalHits), .numHits(numHits));
+
+   initial begin
+   $monitor ($time,,
+            "bcd = %b, sevenSegment = %b",
+            TotalHits, numHits);
+
+        TotalHits = 4'b0000;
+    #10 TotalHits = 4'b0100;
+    #10 TotalHits = 4'b1001;
+    #10 TotalHits = 4'b1111;
+    #10 TotalHits = 4'b0011;
+
+   end
+endmodule : BCDtoSevenSegment_test
+
 module typeMiss
   (input logic [3:0] X, Y,
    input logic Hit,
@@ -150,6 +190,7 @@ endmodule: typeMissTester
 
 module shipHit
     (input logic [3:0] X, Y,
+     input logic [4:0] CurrBiggest,
      output logic [4:0] BiggestShipHit);
 
     logic patrol1, patrol2, battleship, aircraft_c, sub, cruiser;
@@ -205,11 +246,11 @@ module shipHit
 
     always_comb begin 
         if (aircraft_c) BiggestShipHit = 5'b10000;
-        else if (battleship) BiggestShipHit = 5'b01000;
-        else if (cruiser) BiggestShipHit = 5'b00100;
-        else if (sub) BiggestShipHit = 5'b00010;
-        else if (patrol1 | patrol2) BiggestShipHit = 5'b00001;
-        else BiggestShipHit = 5'b00000;
+        else if (battleship && !(CurrBiggest == 5'b10000)) BiggestShipHit = 5'b01000;
+        else if (cruiser && (!(CurrBiggest == 5'b01000) || !(CurrBiggest == 5'b10000))) BiggestShipHit = 5'b00100;
+        else if (sub && (CurrBiggest == 5'b00000 || CurrBiggest == 5'b00001)) BiggestShipHit = 5'b00010;
+        else if ((patrol1 || patrol2) && CurrBiggest == 5'b00000) BiggestShipHit = 5'b00001;
+        else BiggestShipHit = CurrBiggest;
     end
 
 endmodule: shipHit
@@ -218,15 +259,18 @@ endmodule: shipHit
 module shiphit_test();
 
     logic [3:0] X, Y;
+    logic [4:0] CurrBiggest;
     logic [4:0] BiggestShipHit;
     shipHit dut (.*);
 
   initial begin
-    $monitor("X: %h, Y: %h, BiggestShip: %b", X, Y, BiggestShipHit);
-       X = 4'b0011; Y = 4'b0011;
-    #5 X = 4'b0011; Y = 4'b0010;
+    $monitor("X: %h, Y: %h, CurrBiggest: %b BiggestShip: %b", X, Y, CurrBiggest, BiggestShipHit);
+       X = 4'b0011; Y = 4'b0011; CurrBiggest = 5'b00000;
+    #5 X = 4'b0011; Y = 4'b0010; CurrBiggest = 5'b10000;
+    #5 X = 4'b0011; Y = 4'b0010; CurrBiggest = 5'b00000;
     #5 X = 4'b0100; Y = 4'b0001;
-    #5 X = 4'b0010; Y = 4'b1001;
+    #5 X = 4'b0010; Y = 4'b1001; CurrBiggest = 5'b01000;
+    #5 X = 4'b0010; Y = 4'b1001; CurrBiggest = 5'b00000;
     #5 X = 4'b0111; Y = 4'b0110;
     #5 X = 4'b1001; Y = 4'b0001;
     #5 X = 4'b0001; Y = 4'b0001;
@@ -241,6 +285,7 @@ endmodule : shiphit_test
 module isHit
   (input logic [3:0] X, Y,
    input logic [3:0] CurrHits,
+   input logic [4:0] CurrBiggest,
   output logic Hit, nearMiss, Miss,
   output logic [4:0] BiggestShipHit,
   output logic [3:0] TotalHits,
@@ -277,24 +322,96 @@ module isHit
     else TotalHits[3:0] = CurrHits[3:0];
   end
 
+  BCDtoSevenSegment BCD2SevenSegment(.*);
+
 endmodule: isHit
 
 module isHitTester ();
-  logic [3:0] X, Y, CurrHits,
-  logic Hit, nearMiss, Miss,
-  logic [4:0] BiggestShipHit,
-  logic [3:0] TotalHits
-  logic [6:0] numHits);
+  logic [3:0] X, Y, CurrHits;
+  logic Hit, nearMiss, Miss;
+  logic [4:0] BiggestShipHit, CurrBiggest;
+  logic [3:0] TotalHits;
+  logic [6:0] numHits;
+
+  isHit IsHit(.*);
 
   initial begin
-    $monitor($time, " X = %d, Y %d, CurrHits = %d, Hit = %b, nearMiss = %b, \
-             Miss = %b, TotalHits = %d, numHits = %d, BiggestShipHit = %b", 
+    $monitor($time, " X = %d, Y %d, CurrHits = %d, Hit = %b, nearMiss = %b, Miss = %b, \
+             TotalHits = %d, numHits = %b, BiggestShipHit = %b", 
              X, Y, CurrHits, Hit, nearMiss, Miss, TotalHits, 
              numHits, BiggestShipHit);
     X = 4'b0001;
     Y = 4'b0001;
     CurrHits = 4'b0000;
-    #10 
+    CurrBiggest = 5'b00000;
+    #10 Y = 4'b0010;
+    #10 CurrHits = 4'b0011;
+    #10 X = 4'b1001;
+    CurrHits = 4'b0111;
+    #10 $finish;
   end
 
 endmodule: isHitTester
+
+module isBomb 
+  (input logic [3:0] X, Y,
+   input logic [1:0] BigLeft,
+   input logic Big, ScoreThis,
+  output logic [6:0] numHits,
+  output logic [4:0] BiggestShipHit,
+  output logic Hit, nearMiss, Miss, SomethingIsWrong);
+
+  logic [3:0] CurrHits0, CurrHits1, CurrHits2, CurrHits3, CurrHits4;
+  logic [3:0] CurrHits5, CurrHits6, CurrHits7, CurrHits8, CurrHits9;
+  logic [3:0] XLo, XHi, YLo, YHi;
+  logic [4:0] CurrBiggest;
+
+  isWrong IsWrong(.*);
+
+  always_comb begin
+    if (Big) begin
+      XLo = X - 1;
+      XHi = X + 1;
+      YLo = X - 1;
+      YHi = Y + 1;
+    end
+    else begin
+      XLo = 3'd0;
+      XHi = 3'd0;
+      YLo = 3'd0;
+      YHi = 3'd0;
+    end
+  end
+
+  assign CurrHits0 = 4'b0000;
+  assign CurrBiggest = 5'b00000;
+
+  isHit isHit1(.X(X), .Y(Y), .CurrHits(CurrHits0), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits1), .numHits(numHits), 
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit2(.X(X), .Y(Y), .CurrHits(CurrHits1), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits2), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit3(.X(X), .Y(Y), .CurrHits(CurrHits2), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits3), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit4(.X(X), .Y(Y), .CurrHits(CurrHits3), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits4), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit5(.X(X), .Y(Y), .CurrHits(CurrHits4), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits5), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit6(.X(X), .Y(Y), .CurrHits(CurrHits5), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits6), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit7(.X(X), .Y(Y), .CurrHits(CurrHits6), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits7), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit8(.X(X), .Y(Y), .CurrHits(CurrHits7), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits8), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+  isHit isHit9(.X(X), .Y(Y), .CurrHits(CurrHits8), .Hit(Hit), .nearMiss(nearMiss), .Miss(Miss), 
+               .TotalHits(CurrHits9), .numHits(numHits),
+               .BiggestShipHit(BiggestShipHit), .CurrBiggest(CurrBiggest));
+
+endmodule: isBomb
--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,20 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _11006_archive_1.so
-_11006_archive_1.so : archive.6/_11006_archive_1.a
+ARCHIVE_OBJS += _233950_archive_1.so
+_233950_archive_1.so : archive.27/_233950_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_11006_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_233950_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_11006_archive_1.so $@
-
-
-ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.6/_prev_archive_1.a
-	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
-	@rm -f $@
-	@ln -sf .//../simv.daidir//_prev_archive_1.so $@
+	@ln -sf .//../simv.daidir//_233950_archive_1.so $@
 
 
 
